a2090066ce34bcc955ba42f65b1d7c7d07f94cd3
vit9696 <vit9696@users.noreply.github.com>
2017-09-21 20:51:40 +0300

    Fixed compatibility with recent Lilu changes

--- ResourceConverter/main.mm
+++ ResourceConverter/main.mm
@@ -70,7 +70,7 @@ static NSDictionary * generateKexts(NSString *file, NSDictionary *kexts) {
 		
 		[kextPathsSection appendString:makeStringList(@"kextPath", kextIndex, kextPaths)];
 		
-		[kextSection appendFormat:@"\t{ \"%@\", kextPath%zu, %lu, false, %s, {%s}, KernelPatcher::KextInfo::Unloaded },\n",
+		[kextSection appendFormat:@"\t{ \"%@\", kextPath%zu, %lu, {false, %s}, {%s}, KernelPatcher::KextInfo::Unloaded },\n",
 			kextID, kextIndex, [kextPaths count], [kextInfo objectForKey:@"Reloadable"] ? "true" : "false", [kextInfo objectForKey:@"Detect"] ? "true" : ""];
 		
 		[kextNums setObject:[NSNumber numberWithUnsignedLongLong:kextIndex] forKey:kextName];
