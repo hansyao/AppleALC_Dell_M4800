a1bdf8bcc5e7e9846e7e1bfe7c9bda3e9f7d9e0e
vit9696 <vit9696@users.noreply.github.com>
2017-11-27 21:44:47 +0300

    Forced resource generator to optimise and reformat all the resources during building

--- ResourceConverter/generate.sh
+++ ResourceConverter/generate.sh
@@ -5,10 +5,19 @@
 #
 #  Copyright © 2016-2017 vit9696. All rights reserved.
 
-ret=0
-
+# Remove the original resources
 rm -f "${PROJECT_DIR}/AppleALC/kern_resources.cpp"
 
+# Optimise packed layouts and platforms
+"${PROJECT_DIR}/Tools/zlib_unpack.command" || exit 1
+"${PROJECT_DIR}/Tools/zlib_pack.command" -d || exit 1
+
+# Reformat project plists (perl part is for Xcode styling)
+find "${PROJECT_DIR}/Resources/" -name "*.plist" \
+  -exec plutil -convert xml1 {} \; \
+  -exec perl -0777 -pi -e "s#<data>\s+([^\s]+)\s+</data>#<data>\1</data>#g" {} \; || exit 1
+
+ret=0
 "${TARGET_BUILD_DIR}/ResourceConverter" \
 	"${PROJECT_DIR}/Resources" \
 	"${PROJECT_DIR}/AppleALC/kern_resources.cpp" || ret=1
